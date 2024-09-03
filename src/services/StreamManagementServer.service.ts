95e60eb6387906fc5ee855f1efc79c5a691bef32
KanonKC <kanonkce@gmail.com>
2024-09-03 20:59:06 +0700

    Import api base url form env

--- src/services/StreamManagementServer.service.ts
+++ src/services/StreamManagementServer.service.ts
@@ -1,8 +1,10 @@
 import axios, { AxiosPromise } from "axios";
 import { ListAPIQuerystringOprions, ListAPIResponse, TwitchChannelPointRedeemedLog } from "../types/StreamManagementServer.type";
 
+const { VITE_STREAM_MANAGEMENET_SERVER_BASE_URL } = import.meta.env
+
 export async function getAllTwitchChannelPointRedeemed(options?: ListAPIQuerystringOprions): AxiosPromise<ListAPIResponse<TwitchChannelPointRedeemedLog>> {
-    return axios.get('http://localhost:8080/twitch/channel-point-redeemed', {
+    return axios.get(`${VITE_STREAM_MANAGEMENET_SERVER_BASE_URL}/twitch/channel-point-redeemed`, {
         params: options
     })
 }
