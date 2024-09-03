7a41b488a696696ee65a4cb5998c413f224623c5
KanonKC <kanonkce@gmail.com>
2024-09-03 20:59:58 +0700

    Add opacity on list

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -6,7 +6,7 @@ const EventList = ({
     eventList?: string[]
 }) => {
   return (
-    <div className='flex flex-col-reverse'>
+    <div className='flex flex-col-reverse opacity-70'>
         {
             eventList.slice(0, 6).map((event, index) => (
                 <EventListItem key={index} fontColorLevel={index+1} highlight={index+1 === 6}>{ event }</EventListItem>
